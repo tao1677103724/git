752c951b35bf69cbc129ae2b00fe2a83f8b334d9
TAO <1677103724@qq.com>
2023-09-01 11:10:40 +0800

    first commit

---
+++ src/main/java/com/tao/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.tao.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("niubi");
+    }
+    
+//    @Test
+//    public void a(){
+//        System.out.println("two");
+//    }
+}
