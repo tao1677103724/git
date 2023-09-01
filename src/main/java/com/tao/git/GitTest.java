6c8cbe5465b21a51a23d37fda7baf7ec59b1cc29
TAO <1677103724@qq.com>
2023-09-01 18:09:12 +0800

    second commit

--- src/main/java/com/tao/git/GitTest.java
+++ src/main/java/com/tao/git/GitTest.java
@@ -3,6 +3,7 @@ package com.tao.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("niubi");
+        System.out.println("niubi");
     }
     
 //    @Test
